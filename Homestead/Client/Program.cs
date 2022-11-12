4d5c2e44ada750a777ae5b3f9494bf7bfe2dd581
Reese Hodge <reese.hodge@IntelliTect.com>
2022-11-12 08:49:25 -0800

    Add the lobby view model to the service container as a singleton.

--- Homestead/Client/Program.cs
+++ Homestead/Client/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using Homestead.Client;
+using Homestead.Client.ViewModels;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSingleton(typeof(ILobbyVm), typeof(LobbyVm));
 
 await builder.Build().RunAsync();
