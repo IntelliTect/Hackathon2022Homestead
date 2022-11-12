d2ee5e7c911603548d33a843fdba82253589ab1c
Grant Erickson <grant@granterickson.com>
2022-11-11 17:04:17 -0800

    Add Game object

---
+++ Homestead/Shared/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homestead.Shared
+{
+    public class Game
+    {
+        public enum GameState
+        {
+            Joining,
+            Playing,
+            Complete,
+        }
+
+        public string GameId { get; }
+        public GameState State { get; set; }
+        public int? Winner { get; set; }
+        public List<Player> Players { get; }
+        public List<string> DiscardPile { get; } = new List<string>();
+        public int ActivePlayer { get; set; }
+        public List<string> Actions { get; } = new List<string>();
+        public string LastAction { get; set; }
+
+
+
+        public Game()
+        {
+            GameId = Guid.NewGuid().ToString();
+            Players = new List<Player>();
+            // Create 4 players
+            for (int i = 1; i <=4 ; i++)
+            {
+                Players.Add(new Player(i));
+            }
+        }
+    }
+}
---
+++ Homestead/Shared/Player.cs
@@ -0,0 +1,47 @@
+﻿using RandomNameGeneratorLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homestead.Shared
+{
+    public class Player
+    {
+        public enum SkinTones
+        {
+            Light,
+            Dark
+        }
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public int PlayerNumber { get;  }
+        public string Name { get; set; }
+        public SkinTones SkinTone { get; set; }
+        public Genders Gender { get; set; }
+        public List<string> Hand { get; } = new List<string>();
+        public List<string> Board { get; } = new List<string>();
+
+
+        public Player(int playerNumber)
+        {
+            PlayerNumber = playerNumber;
+            var personGenerator = new PersonNameGenerator();
+            Name = personGenerator.GenerateRandomFirstName();
+
+            // Pick random enums
+            Random random = new Random();
+            Array values = Enum.GetValues(typeof(SkinTones));
+            SkinTone = (SkinTones)(values.GetValue(random.Next(values.Length)));
+
+            values = Enum.GetValues(typeof(Genders));
+            Gender = (Genders)(values.GetValue(random.Next(values.Length)));
+
+        }
+    }
+}
