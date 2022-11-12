540d8c90610433faf92d3dfcf4d14445fe2fcf8b
Reese Hodge <reese.hodge@IntelliTect.com>
2022-11-12 11:30:28 -0800

    Created a game controller with a start function

---
+++ Homestead/Server/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Homestead.Server.SignalR;
+using Homestead.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Homestead.Server.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameLookup _lookup;
+        
+        public GameController(IGameLookup lookup)
+        {
+            _lookup = lookup;
+        }
+
+        [HttpPost("/Start/{gameId}")]
+        public Game Start(string gameId)
+        {
+            Game? game = _lookup.GetGame(gameId);
+            if (game == null) throw new ArgumentException("Game not found");
+            game.State = Game.GameState.Playing;
+            return game;
+        }
+    }
+}
