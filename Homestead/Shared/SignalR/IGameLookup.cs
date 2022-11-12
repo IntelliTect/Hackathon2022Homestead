949db1197a4fe72ced81599bc8fcab574f23ab4a
TylerRose <Tyler.Rose@IntelliTect.com>
2022-11-11 20:02:38 -0800

    GameLookup updates

---
+++ Homestead/Shared/SignalR/IGameLookup.cs
@@ -0,0 +1,11 @@
+﻿using Homestead.Shared;
+
+namespace Homestead.Server.SignalR;
+public interface IGameLookup
+{
+    IEnumerable<string> ListGames { get; }
+
+    Game? GetGame(string gameId);
+
+    public bool AddGame(Game game);
+}
