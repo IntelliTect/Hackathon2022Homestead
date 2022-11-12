92063d3ae7df4211427db791c654d4dc166fb4f0
Josh Campbell <Josh.Campbell@IntelliTect.com>
2022-11-11 18:15:47 -0800

    commit

--- Homestead/Server/SignalR/CommunicationHub.cs
+++ Homestead/Server/SignalR/CommunicationHub.cs
@@ -1,19 +1,22 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Homestead.Shared;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Homestead.Server.SignalR;
 
 public class CommunicationHub : Hub
 {
     private IGameEngine engine;
+    private IGameLookup gameLookup;
 
-    public CommunicationHub(IGameEngine engine)
+    public CommunicationHub(IGameEngine engine, IGameLookup gameLookup)
     {
         this.engine = engine;
     }
 
-    public async Task RecieveAction(Action action)
+    public async Task RecieveAction(string gameId, Shared.Action action)
     {
-        var newState = engine.PerformAction(action);
+        var game = gameLookup.Find(gameId);
+        var newState = engine.ProcessAction(game, action);
         
         await Clients.Group(newState.GameId).SendAsync("ActionRecievedAndEvaluated", newState);
     }
