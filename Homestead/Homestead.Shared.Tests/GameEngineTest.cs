9fd134c374a3b5a207a62f0fab013aa8c8e25035
Reese Hodge <reese.hodge@IntelliTect.com>
2022-11-11 19:15:06 -0800

    Cleanup and refactor ProcessAction. Add tests for same.

--- Homestead/Homestead.Shared.Tests/GameEngineTest.cs
+++ Homestead/Homestead.Shared.Tests/GameEngineTest.cs
@@ -1,18 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NuGet.Frameworks;
 
 namespace Homestead.Shared.Tests
 {
+    [TestClass]
     public class GameEngineTest
     {
+        // Do test setup!
+        [TestMethod]
+        public void StartGameTest()
+        { 
+            // break this test up
+            GameEngine engine = new();
+            Game game = engine.Start();
+            Assert.IsNotNull(game);
+            Assert.IsFalse(string.IsNullOrEmpty(game.GameId));
+            Assert.AreEqual(1, game.ActivePlayer);
+            Assert.IsTrue(game.Actions.Any());
+            //CollectionAssert.Contains(game.Actions, Action.ActionType.DrawFromDeck);
+            Assert.AreEqual(Action.ActionType.DrawFromDeck, game.Actions[0].Type);
+            Assert.AreEqual(1, game.Actions[0].PlayerNumber);
+        }
+
+
         [TestMethod]
         public void DrawCardTest()
         {
-            GameEngine engine = new GameEngine();
-            engine.DrawCard();
+            GameEngine engine = new();
+            Game game = engine.Start();
+
+            game = engine.ProcessAction(game, new Action(Action.ActionType.DrawFromDeck, game.ActivePlayer));
+            Assert.IsTrue(game.Players[game.ActivePlayer].Hand.Any());
         }
+
+        [TestMethod]
+        public void DiscardCardTest()
+        {
+            GameEngine engine = new();
+            Game game = engine.Start();
+
+            Action action = new(Action.ActionType.Discard, game.ActivePlayer);
+            action.PlayerCard = Cards.Well;
+
+            game.Players[game.ActivePlayer].Hand.Add(Cards.Well);
+
+            Assert.IsTrue(game.Players[game.ActivePlayer].Hand.Any());
+            game = engine.ProcessAction(game, action);
+            Assert.IsFalse(game.Players[game.ActivePlayer].Hand.Any());
+        }
+
+        // Test for discarding a card not in your hand
     }
 }
