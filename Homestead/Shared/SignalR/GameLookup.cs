2301b548bfc8350fea6835f315f9b86f2c46934c
Grant Erickson <grant@granterickson.com>
2022-11-15 00:16:16 -0500

    Remove games after 30 minutes of inactivity. (#18)

--- Homestead/Shared/SignalR/GameLookup.cs
+++ Homestead/Shared/SignalR/GameLookup.cs
@@ -5,6 +5,8 @@ namespace Homestead.Server.SignalR;
 public class GameLookup : IGameLookup
 {
     ConcurrentDictionary<string, Game> _gameLookup = new();
+    public static TimeSpan TimeUntilGameCleanup { get; set; } = TimeSpan.FromMinutes(30);
+
     public IEnumerable<Game> ListGames { get => _gameLookup.Values.ToList(); }
 
     public Game? GetGame(string gameId)
@@ -18,4 +20,24 @@ public class GameLookup : IGameLookup
         return _gameLookup.TryAdd(game.GameId, game);
     }
 
+    /// <summary>
+    /// Used to remove old or finished games.
+    /// </summary>
+    private static bool _IsLocked = false;
+    private static object _Lock = new();
+    public void CleanupGames()
+    {
+        if (!_IsLocked)
+        {
+            lock (_Lock)
+            {
+                _IsLocked = true;
+                foreach (var game in _gameLookup.Where(g=>g.Value.State == Game.GameState.Complete || DateTime.UtcNow - g.Value.LastPlayDate > TimeUntilGameCleanup).ToList())
+                {
+                    _gameLookup.TryRemove(game);
+                }
+                _IsLocked = false;
+            }
+        }
+    }
 }
